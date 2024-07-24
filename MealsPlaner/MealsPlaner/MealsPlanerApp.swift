ce281206505d53aba9807d06cb1851eb4d1d3c2d
Sofia Cantero <scantero@payfare.com>
2024-07-24 14:59:51 -0300

    xcode project created

---
+++ MealsPlaner/MealsPlaner/MealsPlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsPlanerApp.swift
+//  MealsPlaner
+//
+//  Created by Joseluis SN on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MealsPlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
